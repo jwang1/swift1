0e3646c4fc2c9cb4b0561198ebb6c69ee02ab4cb
jwang1 <sigkdd@yahoo.com>
2018-10-18 22:17:52 -0400

    init check-in need to fix the bug, the hello button title was not changed after alert dialog.

--- swift1/ViewController.swift
+++ swift1/ViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloButton: UIButton!
+    
+    @IBOutlet weak var modifyButtonText: UIButton!
+    
+    @IBAction func modifyButtonText(_ sender: Any) {
+        if self.modifyButtonText.currentTitle == "Changed" {
+            self.modifyButtonText.setTitle("Modify", for: .normal)
+        } else if self.modifyButtonText.currentTitle == "Modify" {
+            self.modifyButtonText.setTitle("Changed", for: .normal)
+        }
+    }
+    
+    
+    @IBAction func showAlert(_ sender: Any) {
+        self.helloButton.setTitle("Test!", for: .normal)
+        
+        let alert = UIAlertController(title: "Hello!", message: "Hello, World!",
+                                      preferredStyle: UIAlertControllerStyle.alert)
+        
+        alert.addAction(UIAlertAction(title: "Close", style:UIAlertActionStyle.default, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
